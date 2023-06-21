9ed9f82ee1fbcb3f7c166007ce0c96e468cd95da
Patryk Lesiak <patryklesiak@student.agh.edu.pl>
2023-06-21 11:12:03 +0200

    Color_ class implementation

--- interp/objects/types/color.py
+++ interp/objects/types/color.py
@@ -1,6 +1,72 @@
 
+from interp.error_handling.exceptions import LogoTomaLogicError
 from interp.objects.types.value import Value_
+
+from interp.objects.types import integer
 
 
 class Color_(Value_):
-    pass
+    def __init__(self, value: tuple):
+        checks = [
+            type(value) is tuple,
+            len(value) == 3,
+            all([type(x) is integer.Integer_ for x in value]),
+        ]
+        if not all(checks):
+            raise ValueError(f"Value {value} cannot be parsed to {type(self)}.")
+        
+        for index, val in enumerate(value):
+            value[index] = val % 256
+
+        def __repr__(self) -> str:
+            return super().__repr__()
+        
+        def __str__(self) -> str:
+            return f"({self.value[0]}, {self.value[1]}, {self.value[2]})"
+        
+        def __add__(self, other):
+            o_Type = type(other)
+            match o_Type.__qualname__:
+                case Color_.__qualname__:
+                    return Color_((self.value[0] + other.value[0], self.value[1] + other.value[1], self.value[2] + other.value[2]))
+                case integer.Integer_.__qualname__:
+                    return Color_((self.value[0] + other.value, self.value[1] + other.value, self.value[2] + other.value))
+                case _:
+                    raise LogoTomaLogicError(f"Cannot add {o_Type} to {type(self)}.")
+                
+        def __sub__(self, other):
+            o_Type = type(other)
+            match o_Type.__qualname__:
+                case Color_.__qualname__:
+                    return Color_((self.value[0] - other.value[0], self.value[1] - other.value[1], self.value[2] - other.value[2]))
+                case integer.Integer_.__qualname__:
+                    return Color_((self.value[0] - other.value, self.value[1] - other.value, self.value[2] - other.value))
+                case _:
+                    raise LogoTomaLogicError(f"Cannot subtract {o_Type} from {type(self)}.")
+                
+        def __mul__(self, other):
+            o_Type = type(other)
+            match o_Type.__qualname__:
+                case integer.Integer_.__qualname__:
+                    return Color_((self.value[0] * other.value, self.value[1] * other.value, self.value[2] * other.value))
+                case _:
+                    raise LogoTomaLogicError(f"Cannot multiply {type(self)} by {o_Type}.")
+                
+        def __truediv__(self, other):
+            o_Type = type(other)
+            match o_Type.__qualname__:
+                case integer.Integer_.__qualname__:
+                    return Color_((self.value[0] // other.value, self.value[1] // other.value, self.value[2] // other.value))
+                case _:
+                    raise LogoTomaLogicError(f"Cannot divide {type(self)} by {o_Type}.")
+                
+        def __mod__(self, other):
+            o_Type = type(other)
+            match o_Type.__qualname__:
+                case integer.Integer_.__qualname__:
+                    return Color_((self.value[0] % other.value, self.value[1] % other.value, self.value[2] % other.value))
+                case _:
+                    raise LogoTomaLogicError(f"Cannot modulo {type(self)} by {o_Type}.")
+        
+        def cast(self, value: tuple):
+            return Color_(value)
