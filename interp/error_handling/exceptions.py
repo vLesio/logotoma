262e3e7c49f761ba7716affcce517317a7c0bc54
Maciexpol <empieniazek@student.agh.edu.pl>
2023-06-21 13:28:06 +0200

    Addded exceotuions

--- interp/error_handling/exceptions.py
+++ interp/error_handling/exceptions.py
@@ -1,6 +1,8 @@
 class LogoTomaException(Exception):
-    def __init__(self, message):
+    def __init__(self, message, line=None, column=None):
         self.message = message
+        self.line = line
+        self.column = column
         
     def __name__(self):
         return self.__class__.__name__
@@ -9,21 +11,21 @@ class LogoTomaException(Exception):
         return repr(self.message)
     
 class LogoTomaSyntaxError(LogoTomaException):
-    def __init__(self, message):
-        super().__init__(message)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
     
 class LogoTomaSemanticError(LogoTomaException):
-    def __init__(self, message):
-        super().__init__(message)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         
 class LogoTomaRuntimeError(LogoTomaException):
-    def __init__(self, message):
-        super().__init__(message)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         
 class LogoTomaValueError(LogoTomaException):
-    def __init__(self, message):
-        super().__init__(message)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
 class LogoTomaLogicError(LogoTomaException):
-    def __init__(self, message):
-        super().__init__(message)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
--- main.py
+++ main.py
@@ -6,8 +6,27 @@ from interp.listener import Listener
 from interp.visitor import Visitor
 from interp.kosmotoma import KosmoToma
 from interp.debugger import debug
+from antlr4.error.ErrorListener import ErrorListener
+from interp.error_handling.exceptions import LogoTomaException, LogoTomaSemanticError, LogoTomaRuntimeError, LogoTomaValueError, LogoTomaLogicError
 
 import pygame
+
+class MyErrorListener( ErrorListener ):
+
+    def __init__(self):
+        super(MyErrorListener, self).__init__()
+
+    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+        raise LogoTomaSemanticError(msg, line=line, column=column)
+
+    # def reportAmbiguity(self, recognizer, dfa, startIndex, stopIndex, exact, ambigAlts, configs):
+    #     raise LogoTomaLogicError("Ambiguity", recognizer=recognizer, dfa=dfa, line=startIndex, stopIndex=stopIndex, exact=exact, ambigAlts=ambigAlts, configs=configs)
+
+    # def reportAttemptingFullContext(self, recognizer, dfa, startIndex, stopIndex, conflictingAlts, configs):
+    #     raise LogoTomaLogicError("Where context", recognizer=recognizer, dfa=dfa, line=startIndex, stopIndex=stopIndex, conflictingAlts=conflictingAlts, configs=configs)
+
+    # def reportContextSensitivity(self, recognizer, dfa, startIndex, stopIndex, prediction, configs):
+    #     raise LogoTomaLogicError("Sensitive context", recognizer=recognizer, dfa=dfa, line=startIndex, stopIndex=stopIndex, prediction=prediction, configs=configs)
 
 if __name__ == "__main__":
     program = None
@@ -23,7 +42,19 @@ if __name__ == "__main__":
     lexer = LogoTomaLexer(data)
     stream = CommonTokenStream(lexer)
     parser = LogoTomaParser(stream)
-    tree = parser.program()
+    parser.addErrorListener( MyErrorListener() )
+    try:
+        tree = parser.program()
+    except LogoTomaException as e:
+                    # line_text 
+            line_number = e.line
+            column = e.column
+            print('\n')
+            print('- ' * 30)
+            print(f"{e.__name__()} occured in line [{line_number}|{column}]:\n{e}\n")
+            
+            quit()
+        
 
     # First run
     # output = ParseTreeWalker().walk(Listener(cmd), tree)
